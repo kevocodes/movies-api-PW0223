201ec3f441182cbc4a5f7558bb61f2646739a63e
kevocodes <00116120@uca.edu.sv>
2024-03-23 20:46:16 -0600

    feat: remove status from error response :fire:

--- src/middlewares/error.middleware.js
+++ src/middlewares/error.middleware.js
@@ -5,7 +5,7 @@ const errorHandler = (err, req, res, next) => {
 
   return res
     .status(err.status || 500)
-    .json({ status: err.status, message: err.message });
+    .json({ message: err.message });
 };
 
 module.exports = { errorHandler };
